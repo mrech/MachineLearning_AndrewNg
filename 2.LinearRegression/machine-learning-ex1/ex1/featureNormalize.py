515ac8b93c72500082daf24c75fb370209f636e3
mrech <rivato.morena@gmail.com>
2019-03-04 20:26:34 +0100

    Implementation note

--- 2.LinearRegression/machine-learning-ex1/ex1/featureNormalize.py
+++ 2.LinearRegression/machine-learning-ex1/ex1/featureNormalize.py
@@ -19,3 +19,14 @@ def featureNormalize(X):
         X_norm[i] = (X[i] - mu[i])/sigma[i]
 
     return X_norm, mu, sigma
+
+
+'''
+IMPLEMENTATION NOTE: When normalizing the features, it is important
+to store the values used for normalization - the mean value and the stan-
+dard deviation used for the computations. After learning the parameters
+from the model, we often want to predict the prices of houses we have not
+seen before. Given a new x value (living room area and number of bed-
+rooms), we must first normalize x using the mean and standard deviation
+that we had previously computed from the training set
+'''
