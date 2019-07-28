e89bd054b1aca4ec6a0d9877bc254b21af0247a8
mrech <rivato.morena@gmail.com>
2019-07-28 13:22:47 +0200

    SVM - Part1 Visualizing Data

---
+++ 7.SupportVectorMachines/machine-learning-ex6/ex6/plotData.py
@@ -0,0 +1,18 @@
+# PLOTDATA Plots the data points X and y into a new figure 
+
+def plotData(X, y):
+    '''
+    PLOTDATA(x,y) plots the data points with + for the positive examples
+    and o for the negative examples. X is assumed to be a Mx2 matrix.
+    '''
+
+    import matplotlib.pyplot as plt
+    import numpy as np
+
+    # Devide Positive and Negative Examples
+    posIndex = np.where(y == 1)[0]
+    negIndex = np.where(y == 0)[0]
+
+    # Plot Examples
+    plt.plot(X[posIndex,0], X[posIndex, 1], 'k+', X[negIndex,0], X[negIndex,1], 'yo')
+    plt.show()
