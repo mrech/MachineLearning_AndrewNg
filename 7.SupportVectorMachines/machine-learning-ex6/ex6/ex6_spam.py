c7bc6734b0500a1596a7b42171fbfec4e624ffd7
mrech <rivato.morena@gmail.com>
2019-08-02 20:22:21 +0200

    ex6_spam Part 5/6 - Top predictor and generalization

--- 7.SupportVectorMachines/machine-learning-ex6/ex6/ex6_spam.py
+++ 7.SupportVectorMachines/machine-learning-ex6/ex6/ex6_spam.py
@@ -7,6 +7,7 @@ from emailFeatures import *
 from scipy.io import loadmat
 from svmTrain import *
 import numpy as np
+from getVocabList import *
 
 # ==================== Part 1: Email Preprocessing ====================
 #  To use an SVM to classify emails into Spam v.s. Non-Spam, you first need
@@ -62,7 +63,7 @@ p = model.predict(X)
 print('Training Accuracy: {:.1f}%\n'.format(
     np.mean((p == y.flatten()).astype(int))*100))
 
-## =================== Part 4: Test Spam Classification ================
+# =================== Part 4: Test Spam Classification ================
 #  After training the classifier, we can evaluate it on a test set. We have
 #  included a test set in spamTest.mat
 
@@ -79,47 +80,51 @@ p = model.predict(Xtest)
 print('Test Accuracy: {:.1f}%\n'.format(
     np.mean((p == ytest.flatten()).astype(int))*100))
 
-'''
-%% ================= Part 5: Top Predictors of Spam ====================
-%  Since the model we are training is a linear SVM, we can inspect the
-%  weights learned by the model to understand better how it is determining
-%  whether an email is spam or not. The following code finds the words with
-%  the highest weights in the classifier. Informally, the classifier
-%  'thinks' that these words are the most likely indicators of spam.
-%
+# ================= Part 5: Top Predictors of Spam ====================
+#  Since the model we are training is a linear SVM, we can inspect the
+#  weights learned by the model to understand better how it is determining
+#  whether an email is spam or not. The following code finds the words with
+#  the highest weights in the classifier. Informally, the classifier
+#  'thinks' that these words are the most likely indicators of spam.
 
-% Sort the weights and obtin the vocabulary list
-[weight, idx] = sort(model.w, 'descend');
-vocabList = getVocabList();
+# Sort the weights and obtin the vocabulary list
+# 'argsort' returns the position on ascending order
 
-fprintf('\nTop predictors of spam: \n');
-for i = 1:15
-    fprintf(' %-15s (%f) \n', vocabList{idx(i)}, weight(i));
-end
+weight = np.sort(model.coef_[0])
+weight = weight[::-1]  # mirror it to get descending order
+idx = np.argsort(model.coef_[0])
+idx = idx[::-1]
 
-fprintf('\n\n');
-fprintf('\nProgram paused. Press enter to continue.\n');
-pause;
+print('\nTop predictors of spam: \n')
 
-%% =================== Part 6: Try Your Own Emails =====================
-%  Now that you've trained the spam classifier, you can use it on your own
-%  emails! In the starter code, we have included spamSample1.txt,
-%  spamSample2.txt, emailSample1.txt and emailSample2.txt as examples.
-%  The following code reads in one of these emails and then uses your
-%  learned SVM classifier to determine whether the email is Spam or
-%  Not Spam
+vocabList = getVocabList()
 
-% Set the file to be read in (change this to spamSample2.txt,
-% emailSample1.txt or emailSample2.txt to see different predictions on
-% different emails types). Try your own emails as well!
-filename = 'spamSample1.txt';
+for i in range(15):
+    print('Weight:', np.round(weight[i],2), ',', 
+          'Word:', [key for key, values in vocabList.items() if values == idx[i]+1])
 
-% Read and predict
-file_contents = readFile(filename);
-word_indices  = processEmail(file_contents);
-x             = emailFeatures(word_indices);
-p = svmPredict(model, x);
 
-fprintf('\nProcessed %s\n\nSpam Classification: %d\n', filename, p);
-fprintf('(1 indicates spam, 0 indicates not spam)\n\n');
-'''
+input('\nProgram paused. Press enter to continue.\n')
+
+## =================== Part 6: Try Your Own Emails =====================
+#  Now that you've trained the spam classifier, you can use it on your own
+#  emails! In the starter code, we have included spamSample1.txt,
+#  spamSample2.txt, emailSample1.txt and emailSample2.txt as examples.
+#  The following code reads in one of these emails and then uses your
+#  learned SVM classifier to determine whether the email is Spam or
+#  Not Spam
+
+# Set the file to be read in (change this to spamSample2.txt,
+# emailSample1.txt or emailSample2.txt to see different predictions on
+# different emails types). Try your own emails as well!
+
+filename = 'spamSample1.txt'
+
+# Read and predict
+file_contents = readFile(filename)
+word_indices = processEmail(file_contents)
+x = emailFeatures(word_indices)
+p = model.predict(x.T)
+
+print('\nProcessed %s\n\nSpam Classification: %d\n' % (filename, p))
+print('(1 indicates spam, 0 indicates not spam)\n\n')
